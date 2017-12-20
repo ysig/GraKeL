3b757410b6cf8243c7af7df3077254834753fb88
ysig <y.siglidis@gmail.com>
2017-12-20 17:33:31 +0200

    trying to fix an error inside appveyor: trying to remove file ending with .

--- install_pynauty.py
+++ install_pynauty.py
@@ -46,7 +46,9 @@ os.system('tar -xf nauty26r10.tar.gz')
 # rename folder
 if (is_windows):
     os.system('rename nauty26r10 nauty')
-    os.remove("nauty/This_is_nauty_26r10.")
+    os.system('dir .')
+    os.system('dir nauty')
+    os.remove("nauty/This_is_nauty_26r10")
 
 if (is_linux):
     os.system('mv nauty26r10 nauty')
