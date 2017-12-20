c82878c1fa28285cb7a39269e33d523c999f18e4
ysig <y.siglidis@gmail.com>
2017-12-20 15:12:29 +0200

    trying to bypass a build error inside install pynauty

--- install_pynauty.py
+++ install_pynauty.py
@@ -46,70 +46,28 @@ os.system('tar -xf nauty26r10.tar.gz')
 # rename folder
 if (is_windows):
     os.system('rename nauty26r10 nauty')
-    overwrite = True
-    exist_cfg_bak = False
-    config_lines = '[build]\ncompiler=mingw32\n[build_ext]\ncompiler=mingw32'
-
-    # For non virtual env propmpt
-    try: input = raw_input
-    except NameError: pass
-    if not venv:
-        print('To setup pynauty the mingw32 compiler must be used.')
-        print('The following lines must added or substituted to "python_install_dir"\\Lib\\distutils\\distutils.cfg')
-        valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-        sys.stdout.write("Do you want to overwrite (if exists)?" + " [y/N]")
-        choice = input().lower()
-        if choice not in valid: 
-            overwrite = False
-        else:
-            overwrite = not valid[choice]
-    
-    # Add build-compiler variables in config.cfg
-    if overwrite:
-        f = False
-        if os.environ.get('PYTHON', '')=='':
-            print('You must set a system variable PYTHON pointing to your python installed directory')
-            f = True
-        elif not os.path.exists(os.environ['PYTHON'] + '\\Lib\\distutils'):
-            f = False
-            sys.stderr.write('The current system variable PYTHON pointing to your python installed directory is invalid\nPython Directory must contain \\Lib\\distutils ..')
-        while f:
-            print('SET PYTHON=')
-            choice = input()
-            f = os.path.exists(str(choice) + '\\Lib\\distutils')
-            if not f:
-                sys.stderr.write(str(choice) + ' is not a valid Python directory\nPython Directory must contain \\Lib\\distutils ..')
-            else:
-                os.system('set PYTHON='+str(choice))
-                os.environ['PYTHON'] = str(choice)
-        config_path = str(os.environ['PYTHON']) + '\\Lib\\distutils\\distutils.cfg'
-        if os.path.exists(config_path):
-            warning.warn('File distutils.cfg exists..')
-            os.system('copy ' + config_path + ' ' + config_path + '.bak')
-            warning.warn('Overwriting .. [Backup saved on "distutils.cfg.bak"]')
-            os.system('del ' + config_path)
-            exist_cfg_bak = True
-        config_cfg = open(config_path, 'w+')
-        config_cfg.write('[build]\ncompiler=mingw32\n[build_ext]\ncompiler=mingw32')
-        config_cfg.close()
         
 if (is_linux):
     os.system('mv nauty26r10 nauty')
 
 # build pynauty
-os.system('make pynauty')
+if is_windows:
+    os.system('make nauty-objects')
+    os.system('python setup.py build --compiler=mingw32')
+if is_linux:
+    os.system('make pynauty')
 
-# define if insidd virtual-env and install
+# define if inside virtual-env and install
 if venv:
-    os.system('make virtenv-ins')
+    if is_linux:
+        os.system('make virtenv-ins')
+    if is_windows:
+        os.system('pip install --upgrade .')
 else:
-    os.system('make tests')
-    os.system('make user-ins')
-
+    if is_linux:
+        os.system('make user-ins')
+    if is_windows:
+        os.system('pip install --user --upgrade .')
 # exit directory and delete
 os.chdir("..")
 shutil.rmtree('pynauty-0.6.0')
-if is_windows and exists_cfg_bak:
-    warning.warn('Restoring: distutils.cfg.bak -> distutils.cfg')
-    os.system('del ' + config_path)
-    os.system('rename ' + config_path + '.bak ' + config_path)
