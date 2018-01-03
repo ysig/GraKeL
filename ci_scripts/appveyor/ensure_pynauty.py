5b142b80579f1d776952cfc506258b050c340927
ysig <y.siglidis@gmail.com>
2018-01-03 18:40:02 +0100

    +log messages
    ..

--- ci_scripts/appveyor/ensure_pynauty.py
+++ ci_scripts/appveyor/ensure_pynauty.py
@@ -1,8 +1,9 @@
+import traceback
 try:
     import pynauty
     print('Succesful import of pynauty!')
 except ImportError as ierr:
     print('Import-Error:', ierr, '..')
-    print('Traceback\n' + str(traceback.print_exc()))
+    print('Traceback\n---------\n' + str(traceback.print_exc()))
 except Exception as ex:
 	print('Other exception:',ex)
