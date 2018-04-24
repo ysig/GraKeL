6ba975fedfeedca058fd69de6c02f1eb8da16cc9
ysig <y.siglidis@gmail.com>
2018-04-24 18:22:48 +0200

    Trying a fix in circleci and travis.

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -1,17 +1,18 @@
 set -e
-
-# Get into a temp directory to run test from the installed scikit learn and
-# check if we do not leave artifacts
-mkdir -p $TEST_DIR
-
-cd $TEST_DIR
+current_dir="$(pwd)"
 
 if [[ "$COVERAGE" == "true" ]]; then
+    # Store artifacts: user is mounted on host
+    mkdir -p "/host$TEST_DIR"
+    cd "/host$TEST_DIR"
     pwd
     ls
     nosetests --with-coverage --cover-package=$MODULE $MODULE
 else
+    # Ignore arifacts: just change folder
+    mkdir -p $TEST_DIR    
+    cd $TEST_DIR
     nosetests $MODULE
 fi
 
-cd $TRAVIS_BUILD_DIR
+cd $current_dir
