2479f442306857f195afe6d003ba831ef68af8d6
giannisnik <giannisnik@hotmail.com>
2018-05-28 20:10:12 +0200

    modified examples

--- examples/plot_wl_subtree_MUTAG.py
+++ examples/plot_wl_subtree_MUTAG.py
@@ -1,6 +1,6 @@
 """
 =============================================================
-Classification of the MUTAG dataset using the WL-subtree kernel.
+Classification on the MUTAG dataset using the WL-subtree kernel.
 =============================================================
 
 An example plot of :class:`grakel.GraphKernel`, :class:`grakel.WeisfeilerLehman`, :class:`grakel.VertexHistogram`
