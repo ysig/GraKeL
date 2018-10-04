528d38839f4a8a8f71c4cb9e2b3592996bf8d2af
ysig <y.siglidis@gmail.com>
2018-10-04 12:15:14 +0200

    Trying a fix on backdating of coverage. Set build wheel flag to false appveyor.

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -2,17 +2,12 @@ set -e
 current_dir="$(pwd)"
 
 if [[ "$COVERAGE" == "true" ]]; then
-    # Store artifacts: user is mounted on host
-    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        mkdir -p "/host$TEST_DIR";
-        cd "/host$TEST_DIR";
-    else
-        mkdir -p $TEST_DIR;
-        cd $TEST_DIR;
-    fi
+    # Store artifacts
+    mkdir -p $TEST_DIR;
+    cd $TEST_DIR;
     pwd;
-    ls;
     nosetests --with-coverage --cover-package=$MODULE $MODULE;
+    ls -x -l;
 else
     # Ignore arifacts: just change folder
     mkdir -p $TEST_DIR;
