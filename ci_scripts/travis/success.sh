e46fe8f34c6ff340576f11c230071881c64ddd7d
ysig <y.siglidis@gmail.com>
2018-10-04 20:34:24 +0200

    Trying a fix for codecov

--- ci_scripts/travis/success.sh
+++ ci_scripts/travis/success.sh
@@ -3,14 +3,14 @@ set -e
 if [[ "$COVERAGE" == "true" ]]; then
     # Need to run coveralls from a git checkout, so we copy .coverage
     # from TEST_DIR where nosetests has been run
-    cp $TEST_DIR/.coverage $TRAVIS_BUILD_DIR
+    cp $TEST_DIR/coverage.xml $TRAVIS_BUILD_DIR
     cd $TRAVIS_BUILD_DIR
     # Ignore coveralls failures as the coveralls server is not
     # very reliable but we don't want travis to report a failure
     # in the github UI just because the coverage report failed to
     # be published.
     $PYTHON -m pip install codecov
-    $PYTHON -m codecov || echo "Codecov upload failed"
+    $PYTHON -m codecov
 fi
 
 if [[ "$DEPLOY_WHEEL" == "true" ]]; then
