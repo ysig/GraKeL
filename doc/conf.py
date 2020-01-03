7b839ba4e18d004689802f441e49eac173090cd7
Giannis Nikolentzos <giannisnik@hotmail.com>
2020-01-03 23:58:48 +0200

    Updated documentation [skip appveyor] [skip travis]

--- doc/conf.py
+++ doc/conf.py
@@ -125,7 +125,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GraKeL'
-copyright = u'2018, DaSciM (BSD License)'
+copyright = u'2019, DaSciM (BSD License)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
