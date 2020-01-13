75063c2fe8792e108cb21f567fb3c6af2a1a7ab6
ysig <y.siglidis@gmail.com>
2020-01-13 13:10:47 +0200

    Fixing version for sphinx and doctesting

--- ci_scripts/circleci/install.sh
+++ ci_scripts/circleci/install.sh
@@ -15,7 +15,7 @@ sudo apt-get install build-essential python-dev python-setuptools > /dev/null
 # Setup a python venv and install basics
 source ./venv/bin/activate
 pip install --upgrade pip
-pip install --upgrade pandas networkx matplotlib setuptools nose coverage "sphinx==1.8.5" pillow sphinx-gallery sphinx_rtd_theme sphinxcontrib-bibtex nb2plots numpydoc tqdm > /dev/null
+pip install --upgrade pandas networkx matplotlib setuptools nose coverage "sphinx>=2.0.0" pillow sphinx-gallery sphinx_rtd_theme sphinxcontrib-bibtex nb2plots numpydoc tqdm > /dev/null
 pip install -r requirements.txt > /dev/null
 pip install "cvxopt==1.2.0" > /dev/null
 
@@ -29,6 +29,6 @@ python setup.py develop
 
 
 # Build Docs
-set -o pipefail && cd doc && make clean html docteset 2>&1 | tee ~/log.txt && cd ..
+set -o pipefail && cd doc && make clean html doctest 2>&1 | tee ~/log.txt && cd ..
 cat ~/log.txt && if grep -q "Traceback (most recent call last):" ~/log.txt; then false; else true; fi
 
