38917bd6304c04df2c150c7801522ea52abac878
ysig <y.siglidis@gmail.com>
2020-02-22 18:10:16 +0200

    deploy sdist differentiation from deploy wheel

--- ci_scripts/travis/success.sh
+++ ci_scripts/travis/success.sh
@@ -1,17 +1,17 @@
 set -e
-    
-if [[ "$DEPLOY_WHEEL" == "true" ]]; then
-    cd $TRAVIS_BUILD_DIR
-    rm -rf dist/
+cd $TRAVIS_BUILD_DIR
+rm -rf dist/
 
-    $PIP install twine
-    $PIP install setuptools --upgrade
-    if [[ "$DEPLOY_SDIST" == "true" ]]; then
-        # Build & Deploy sdist
-        $PYTHON setup.py sdist --formats=zip 
-        $PYTHON -m twine upload dist/*.zip || true
-    fi
+$PIP install twine
+$PIP install setuptools --upgrade
 
+if [[ "$DEPLOY_SDIST" == "true" ]]; then
+    # Build & Deploy sdist
+    $PYTHON setup.py sdist --formats=zip 
+    $PYTHON -m twine upload dist/*.zip || true
+fi
+
+if [[ "$DEPLOY_WHEEL" == "true"  ]]; then
     # Deploy wheels
     $PYTHON -m twine upload $WHEEL_FOLDER/*.whl || true
 fi
