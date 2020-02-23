32e036161eb435727d92ffd999be14c36d1d7f33
ysig <y.siglidis@gmail.com>
2020-02-23 15:39:08 +0200

    support for hg

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -3,12 +3,15 @@ current_dir="$(pwd)"
 
 if [[ "$COVERAGE" == "true" ]]; then
     # Store artifacts
+    sudo add-apt-repository -y ppa:mercurial-ppa/releases
+    sudo apt-get update
+    sudo apt-get install -y mercurial
     pip install coverage
     pip install codecov
     nosetests $MODULE --with-coverage --cover-package=$MODULE;
     echo "Coverage";
     ls -la;
-  
+
     # Ignore coveralls failures as the coveralls server is not
     # very reliable but we don't want travis to report a failure
     # in the github UI just because the coverage report failed to
