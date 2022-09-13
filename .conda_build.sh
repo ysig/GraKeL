747a8e7121a500c38b0f6754c65e2cf493611329
eddiebergman <eddiebergmanhs@gmail.com>
2022-09-13 14:04:50 +0200

    readd conda build command for python 2.7

--- .conda_build.sh
+++ .conda_build.sh
@@ -3,7 +3,7 @@ set -x
 rm -rf conda_build
 mkdir -p conda_build
 conda build purge
-# conda-build . --output-folder conda_build/ --python 2.7
+conda-build . --output-folder conda_build/ --python 2.7
 conda-build . --output-folder conda_build/ --python 3.5
 conda-build . --output-folder conda_build/ --python 3.6
 conda-build . --output-folder conda_build/ --python 3.7
