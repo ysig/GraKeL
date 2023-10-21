2d029f000b2136d10f7921505009c68558bb26d3
ysig <y.siglidis@gmail.com>
2023-10-21 20:17:26 +0200

    Updating versions of python and cvxopt carpetfix

--- .conda_build.sh
+++ .conda_build.sh
@@ -10,4 +10,6 @@ conda-build . --output-folder conda_build/ --python 3.7
 conda-build . --output-folder conda_build/ --python 3.8
 conda-build . --output-folder conda_build/ --python 3.9
 conda-build . --output-folder conda_build/ --python 3.10
+conda-build . --output-folder conda_build/ --python 3.11
+conda-build . --output-folder conda_build/ --python 3.12
 conda convert -f --platform all conda_build/linux-64/*.tar.bz2 -o conda_build
